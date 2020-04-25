e4fd8affd4b2a7d93ec077f478e0e7e3e0d67c7a
metr0jw <webmaster@kw.ac.kr>
2020-04-26 02:43:18 +0900

    Add preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -2,10 +2,13 @@ import pathlib
 import os
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 import tensorflow as tf
 import pandas as pd
 import numpy as np
+
 
 train_file_path = ("./dataset/original/")
 preprocessed_file_path = ("./dataset/preprocessed/")
@@ -13,10 +16,30 @@ preprocessed_file_path = ("./dataset/preprocessed/")
 train_file_challenger = pd.read_csv(train_file_path + "Challenger_Ranked_Games.csv").iloc[:, 1:]
 train_file_grandmaster = pd.read_csv(train_file_path + "GrandMaster_Ranked_Games.csv").iloc[:, 1:]
 train_file_master = pd.read_csv(train_file_path + "Master_Ranked_Games.csv").iloc[:, 1:]
-train_file = pd.concat([train_file_challenger, train_file_grandmaster, train_file_master], axis=0)
+train_file = pd.concat([train_file_challenger, train_file_grandmaster, train_file_master], axis=0).reset_index(drop=True)
 
-cols_to_move = ['blueWins', 'redWins']
-new_cols = np.hstack((train_file.columns.difference(cols_to_move), cols_to_move))
+num_of_features = 47
+cols_to_remove = ['blueWins', 'redWins']
 
-train_file = train_file.reindex(columns=new_cols)
+winner = list()
+for i in train_file.index:
+    if train_file['blueWins'].iloc[i] == 0:
+        winner.append('blue')
+    else:
+        winner.append('red')
+winner = pd.DataFrame(winner, columns=['winner'])
+train_file = pd.concat([train_file, winner], axis=1)
+train_file = train_file.drop(cols_to_remove, axis=1)
+
+num_pipeline = Pipeline([
+
+])
+
+str_pipeline = Pipeline([
+
+])
+
+full_pipeline = Pipeline([
+
+])
 
