df858ec6adf7e8afadc4ec787434f751876ffd60
metr0jw <webmaster@kw.ac.kr>
2020-04-30 23:34:19 +0900

    Seperate ANN and using ML algorithm

---
+++ train_logistic.py
@@ -0,0 +1,7 @@
+import numpy as np
+import os
+import pandas as pd
+import seaborn as sns
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, KFold
+from matplotlib import pyplot as plt
